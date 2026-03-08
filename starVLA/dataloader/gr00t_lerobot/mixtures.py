44399c6788e303c7c3a50a9577331f2cc56342b7
outsider86 <wangpc@berkeley.edu>
2026-03-08 01:52:41 +0000

    dd

--- starVLA/dataloader/gr00t_lerobot/mixtures.py
+++ starVLA/dataloader/gr00t_lerobot/mixtures.py
@@ -225,10 +225,6 @@ DATASET_NAMED_MIXTURES = {
         ("adjust_bottle", 1.0, "robotwin"),
     ],
 
-    "fastumi_pickandplace_real_0307": [
-        ("pickandplace-real-0307", 1.0, "fastumi"),
-    ],
-
     "multi_robot": [
         ("LEROBOT_LIBERO_DATA/libero_10_no_noops_1.0.0_lerobot", 1.0, "libero_franka"),
         # ("OXE_LEROBOT_DATASET/bridge_orig_1.0.0_lerobot", 1.0, "oxe_bridge"),
